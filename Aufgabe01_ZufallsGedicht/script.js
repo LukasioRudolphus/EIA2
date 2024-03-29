4d3e21460a5a592305af37fa248a3e0db549f23e
LukasioRudolphus <139594605+LukasioRudolphus@users.noreply.github.com>
2024-03-29 14:27:35 +0100

    Aufgabe01_ZufallsGedicht

--- Aufgabe01_ZufallsGedicht/script.js
+++ Aufgabe01_ZufallsGedicht/script.js
@@ -1,18 +1,19 @@
 "use strict";
 var ZufallsGedicht;
 (function (ZufallsGedicht) {
-    let subjekt = ["Gunther", "Die fette alte Katze", "Das Handball Team", "Der Laptop", "Das Paar"];
-    let prädikat = [" beißt ", " jagt ", " springt ", " baut ", " trägt "];
-    let objekt = ["die Katze.", "den Hund.", "ins Schwimmbecken.", "den Code.", "die Kiste."];
-    // console.log(subjekt, prädikat, objekt);
+    let subjekt = ["Gunther", "Die fette Katze", "Das Handball-Team", "Der Hausmeister", "Das Paar"];
+    let prädikat = [" beißt ", " jagt ", " baut ", " springt auf ", " trägt "];
+    let objekt = ["die Tastatur.", "den Hund.", "das Schwimmbecken.", "das Dach.", "die Kiste."];
     for (let i = 5; i >= 1; i--) {
-        // console.log(i);
         let word = getVerse(subjekt, prädikat, objekt);
         console.log(word);
     }
     function getVerse(_sub, _prä, _obj) {
-        // let zufallsWert: number = (Math.floor(Math.random()*5));
-        let satz = _sub[Math.floor(Math.random() * 5)] + _prä[Math.floor(Math.random() * 5)] + _obj[Math.floor(Math.random() * 5)];
+        let satz = "";
+        let auswahlsub = Math.floor(Math.random() * _sub.length);
+        let auswahlprä = Math.floor(Math.random() * _prä.length);
+        let auswahlobj = Math.floor(Math.random() * _obj.length);
+        satz = _sub.splice(auswahlsub, 1)[0] + _prä.splice(auswahlprä, 1)[0] + _obj.splice(auswahlobj, 1)[0];
         return satz;
     }
 })(ZufallsGedicht || (ZufallsGedicht = {}));
--- Aufgabe01_ZufallsGedicht/script.ts
+++ Aufgabe01_ZufallsGedicht/script.ts
@@ -1,19 +1,19 @@
 namespace ZufallsGedicht {
-    let subjekt: string[] = ["Gunther", "Die fette alte Katze", "Das Handball Team", "Der Laptop", "Das Paar"];
-    let prädikat: string[] = [" beißt ", " jagt ", " springt ", " baut ", " trägt "];
-    let objekt: string[] = ["die Katze.", "den Hund.", "ins Schwimmbecken.", "den Code.", "die Kiste."]; 
-
-    // console.log(subjekt, prädikat, objekt);
+    let subjekt: string[] = ["Gunther", "Die fette Katze", "Das Handball-Team", "Der Hausmeister", "Das Paar"];
+    let prädikat: string[] = [" beißt ", " jagt ", " baut ", " springt auf ", " trägt "];
+    let objekt: string[] = ["die Tastatur.", "den Hund.", "das Schwimmbecken.", "das Dach.", "die Kiste."];
 
     for (let i = 5; i >= 1; i--) {
-        // console.log(i);
         let word: string = getVerse(subjekt, prädikat, objekt);
         console.log(word);
     }
 
-    function getVerse (_sub: string[], _prä: string[], _obj: string[]) {
-        // let zufallsWert: number = (Math.floor(Math.random()*5));
-        let satz = _sub[Math.floor(Math.random()*5)] + _prä[Math.floor(Math.random()*5)] + _obj[Math.floor(Math.random()*5)];
+    function getVerse(_sub: string[], _prä: string[], _obj: string[]) {
+        let satz: string = "";
+        let auswahlsub: number = Math.floor(Math.random() * _sub.length);
+        let auswahlprä: number = Math.floor(Math.random() * _prä.length);
+        let auswahlobj: number = Math.floor(Math.random() * _obj.length);
+        satz = _sub.splice(auswahlsub, 1)[0] + _prä.splice(auswahlprä, 1)[0] + _obj.splice(auswahlobj, 1)[0];
         return satz;
     }
 
