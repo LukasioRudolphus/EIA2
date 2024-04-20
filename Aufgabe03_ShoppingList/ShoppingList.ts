ac19cc133c9a35f558855311aa6e46e7d59048dd
LukasioRudolphus <139594605+LukasioRudolphus@users.noreply.github.com>
2024-04-20 14:16:25 +0200

    Einkaufen

---
+++ Aufgabe03_ShoppingList/ShoppingList.ts
@@ -0,0 +1,21 @@
+namespace shoppingList {
+    // window.addEventListener("load", nowLoad);
+
+    // function nowLoad(_event: Event): void {
+    //     console.log("jo");
+    //     document.getElementById("first")?.addEventListener("input", fitWidth);
+    //     // let inputAmount = document.getElementsByClassName(".amount");
+    //     // for (let i = 0; i < inputAmount.length; i++) {
+    //     //     inputAmount[i].addEventListener("change", fitWidth);
+    //     //     console.log("hi");
+    //     // }
+    // }
+
+    // function fitWidth(this: any): void {
+    //     console.log("nenene");
+    //     let that = document.getElementById("first");
+    //     if (that != null) {
+    //         that.style.width = input.value + "px";
+    //     }
+    // }
+}
