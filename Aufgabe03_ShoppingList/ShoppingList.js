aaaa00eb2bbdaa86dcf3dc5b629369bafc9b2af9
LukasioRudolphus <139594605+LukasioRudolphus@users.noreply.github.com>
2024-04-21 04:44:55 +0200

    L03_done

--- Aufgabe03_ShoppingList/ShoppingList.js
+++ Aufgabe03_ShoppingList/ShoppingList.js
@@ -1,2 +1,42 @@
 "use strict";
+var shoppingList;
+(function (shoppingList) {
+    window.addEventListener("load", nowLoad);
+    function nowLoad(_event) {
+        console.log("jo");
+        let fieldAmount = document.getElementsByClassName("amount");
+        for (let i = 0; i < fieldAmount.length; i++) {
+            fieldAmount[i].addEventListener("input", logAmount);
+            console.log("hi");
+        }
+        let fieldCheck = document.getElementsByClassName("bought");
+        console.log(fieldCheck);
+        for (let i = 0; i < fieldCheck.length; i++) {
+            fieldCheck[i].addEventListener("click", logCheck);
+            console.log("ho");
+        }
+        let fieldDelete = document.getElementsByClassName("deleteEntry");
+        for (let i = 0; i < fieldDelete.length; i++) {
+            fieldDelete[i].addEventListener("click", logDelete);
+            console.log("he");
+        }
+        let fieldComment = document.getElementsByClassName("comment");
+        for (let i = 0; i < fieldComment.length; i++) {
+            fieldComment[i].addEventListener("change", logComment);
+            console.log("he");
+        }
+    }
+    function logAmount() {
+        console.log("Anzahl: " + this.value);
+    }
+    function logCheck(_event) {
+        console.log("Kauf wurde vollzogen.");
+    }
+    function logDelete(_event) {
+        console.log("Delete?");
+    }
+    function logComment() {
+        console.log("Kommentar: " + this.value);
+    }
+})(shoppingList || (shoppingList = {}));
 //# sourceMappingURL=ShoppingList.js.map
--- Aufgabe03_ShoppingList/ShoppingList.ts
+++ Aufgabe03_ShoppingList/ShoppingList.ts
@@ -1,21 +1,45 @@
 namespace shoppingList {
-    // window.addEventListener("load", nowLoad);
+    window.addEventListener("load", nowLoad);
 
-    // function nowLoad(_event: Event): void {
-    //     console.log("jo");
-    //     document.getElementById("first")?.addEventListener("input", fitWidth);
-    //     // let inputAmount = document.getElementsByClassName(".amount");
-    //     // for (let i = 0; i < inputAmount.length; i++) {
-    //     //     inputAmount[i].addEventListener("change", fitWidth);
-    //     //     console.log("hi");
-    //     // }
-    // }
+    function nowLoad(_event: Event): void {
+        console.log("jo");
+        let fieldAmount = document.getElementsByClassName("amount");
+        for (let i = 0; i < fieldAmount.length; i++) {
+            fieldAmount[i].addEventListener("input", logAmount);
+            console.log("hi");
+        }
+        let fieldCheck = document.getElementsByClassName("bought");
+        console.log(fieldCheck);
+        for (let i = 0; i < fieldCheck.length; i++) {
+            fieldCheck[i].addEventListener("click", logCheck);
+            console.log("ho");
+        }
+        let fieldDelete = document.getElementsByClassName("deleteEntry");
+        for (let i = 0; i < fieldDelete.length; i++) {
+            fieldDelete[i].addEventListener("click", logDelete);
+            console.log("he");
+        }
+        let fieldComment = document.getElementsByClassName("comment");
+        for (let i = 0; i < fieldComment.length; i++) {
+            fieldComment[i].addEventListener("change", logComment);
+            console.log("he");
+        }
+    }
 
-    // function fitWidth(this: any): void {
-    //     console.log("nenene");
-    //     let that = document.getElementById("first");
-    //     if (that != null) {
-    //         that.style.width = input.value + "px";
-    //     }
-    // }
+    function logAmount(this: any): void {
+        console.log("Anzahl: " + this.value);
+    }
+
+    function logCheck(_event: Event): void {
+        console.log("Kauf wurde vollzogen.");
+    }
+
+    function logDelete(_event: Event): void {
+        console.log("Delete?");
+    }
+
+    function logComment(this: any): void {
+        console.log("Kommentar: " + this.value);
+    }
+
 }
